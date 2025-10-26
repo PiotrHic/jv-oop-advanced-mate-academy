9b41bf8c370952cf70d54b9e49ee58c88a9ee4fa
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-10-26 08:59:55 +0100

    Swuare Class

---
+++ src/main/java/core/basesyntax/Square.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+public class Square extends Figure implements Draw{
+
+    public Square(int square_wall) {
+        this.square_wall = square_wall;
+        this.square_area = square_wall*square_wall;
+    }
+
+    int square_wall;
+    double square_area;
+
+    @Override
+    public void draw() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Square has a color: ").append(color).append(" and square area of: ")
+                .append(square_wall).append(" m2");
+        System.out.println();
+    }
+}
